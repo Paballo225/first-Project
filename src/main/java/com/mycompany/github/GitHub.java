9e093d42a09ce3fd0b37f1df0bf602ce6f3da15b
RC_Student_lab <RC_Student_lab@10.0.0.27>
2024-09-10 12:00:03 +0000

    Created application that adds two numbers

--- src/main/java/com/mycompany/github/GitHub.java
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -13,6 +13,8 @@ public class GitHub {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        int c = a+b;
+        int c = a + b;
+        
+        System.out.println(c);
     }
 }
